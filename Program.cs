136f0c8600c17ff6061cfd67f9888537ddfb1ff1
ABWD396 <walldaus296@gmail.com>
2024-10-24 10:23:10 +0300

    L1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorldProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!!!");
+
+        Console.ReadKey();
+
+        Console.WriteLine("Hello ");
+        Console.WriteLine("World");
+        Console.WriteLine("!!!");
+
+        Console.ReadLine();
+    }
+}
